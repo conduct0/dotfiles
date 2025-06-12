9ad8da73b3d017762cad2ba9e6f74c585e6cde19
conduct0 <conduct0@users.noreply.github.com>
2025-06-12 11:14:27 +0200

    chore(nvim): improved python ux, came back to pyright and use ruff now

--- nvim/lua/custom/plugins/conform-nvim.lua
+++ nvim/lua/custom/plugins/conform-nvim.lua
@@ -27,8 +27,14 @@ return { -- Autoformat
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
-			python = { "isort", "black", "ruff" },
-			--
+			python = {
+				-- To fix auto-fixable lint errors.
+				"ruff_fix",
+				-- To run the Ruff formatter.
+				"ruff_format",
+				-- To organize the imports.
+				"ruff_organize_imports",
+			},
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
 			javascript = { "prettierd", "prettier", stop_after_first = true },
