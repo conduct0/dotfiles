29062cce4a85c502419a18763398789f5b73e562
conducto <43495400+conduct0@users.noreply.github.com>
2026-07-14 11:53:49 +0200

    fix(tmux): tmux has-session exact string match

--- tmux/tmux-sessionizer.sh
+++ tmux/tmux-sessionizer.sh
@@ -15,7 +15,8 @@ if [ -z "$session" ]; then
     exit 0
 fi
 
-if ! tmux has-session -t "$session_name" 2> /dev/null; then
+# names have to start with = so that exact match happens in if above
+if ! tmux has-session -t "=$session_name" 2> /dev/null; then
     tmux new-session -s "$session_name" -c "$session" -d
 fi
 
